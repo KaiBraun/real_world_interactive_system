433fbfabed6dd0fd40895e258254c99c26f169bb
KaiBraun <45015016+KaiBraun@users.noreply.github.com>
2024-11-19 13:08:11 +0900

    work on welcome view

---
+++ party_app/lib/shared/constants.dart
@@ -0,0 +1,10 @@
+//Colors
+
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+class Constants {
+  //static const greyColor = Color(0xff696969);
+  //static const blackColor = Color(0xff353535);
+}
--- party_app/lib/ui/welcome_view.dart
+++ party_app/lib/ui/welcome_view.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 
+import '../shared/utils.dart';
+
 class WelcomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.onSecondaryFixed,
 
-      ),
-      body: Column(
-        children: [
-          Text("Test123423412",style: TextStyle(color: Colors.red),),
-          ElevatedButton(
-              onPressed: () => {
+      body: Container(
+        color: Theme.of(context).colorScheme.primary,
+        height: Utils.getHeight(context),
+        width: Utils.getWidth(context),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Test123423412",style: TextStyle(color: Colors.black12),),
+            ElevatedButton(
+                onPressed: () => {
 
-              },
-              child: Text("New Player")
-          )
-        ],
+                },
+                child: Text("Start new game")
+            )
+          ],
+        ),
       )
 
     );
