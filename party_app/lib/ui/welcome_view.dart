b5bb0c0c208cdfb53ab7b1230a86f457d60172a9
Bealopes23 <beatriz.coelho.lopes@keio.jp>
2024-12-15 17:59:53 +0900

    viewpage update

--- party_app/lib/ui/welcome_view.dart
+++ party_app/lib/ui/welcome_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../shared/constants.dart';
 import '../shared/utils.dart';
 import 'add_players_view.dart';
@@ -8,24 +7,115 @@ class WelcomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-      color: Constants.primaryColor,
-      height: Utils.getHeight(context),
-      width: Utils.getWidth(context),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Warning: This game may cause questionable life choices!"),
-          ElevatedButton(
-              onPressed: () => {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => AddPlayersView())),
-                  },
-              child: Text("Start new game"))
-        ],
+      body: Container(
+        color: Constants.primaryColor, // Yellow background
+        height: Utils.getHeight(context),
+        width: Utils.getWidth(context),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Warning Text at the Top
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                "Warning: This game may cause questionable life choices!",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w600,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+
+            SizedBox(height: 80),
+
+            // Logo Title and Emoji
+            Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Raise ",
+                    style: TextStyle(
+                      fontSize: 48,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  WidgetSpan(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 4.0),
+                      child: Icon(
+                        Icons.sports_bar_rounded,
+                        size: 48,
+                        color: Colors.orangeAccent,
+                      ),
+                    ),
+                  ),
+                  TextSpan(
+                    text: " your glass",
+                    style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+              textAlign: TextAlign.center,
+            ),
+
+            SizedBox(height: 50),
+
+            // Start Button
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 12),
+                elevation: 3,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AddPlayersView(),
+                  ),
+                );
+              },
+              child: Text(
+                "Start",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+
+            SizedBox(height: 100),
+
+            // Footer Text
+            Text(
+              "Life’s too short for boring parties!",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
+
+
+
+
+
+
+
