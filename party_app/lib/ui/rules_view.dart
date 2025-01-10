a3d6c087c08879519b89a009ea1cfe6ee21b2a04
Bealopes23 <beatriz.coelho.lopes@keio.jp>
2025-01-10 19:56:14 +0900

    Rules view

--- party_app/lib/ui/rules_view.dart
+++ party_app/lib/ui/rules_view.dart
@@ -1,55 +1,63 @@
 import 'package:flutter/material.dart';
 
-
 class RulesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final List<String> rules = [
+      "The Knights of 3 have to drink every time a player rolls the number 3!",
+      "The House Elf is the player who has drunk less and bla bla bla...",
+      "The King of the castle is the player who has drunk more and...",
+      "Every time someone rolls a total sum of 7, the previous player has to drink, and the current player rolls the dice again.",
+      "Every time someone rolls a total sum of 8, everyone has to drink, and the current player rolls the dice again.",
+      "Every time someone rolls a total sum of 9, the previous player has to drink, and the current player rolls the dice again.",
+      "Every time someone makes people drink three times in a row, a new rule for the game must be set by this player.",
+      "Every time a player rolls double, a duel between two players is set.",
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Game Rules"),
+        title: Text(
+          "Game Rules",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 26,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Lemon',
+            color: Colors.red,
+          ),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            Text(
-              "Game Rules:",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            Text(
-              "1. The Knights of 3 have to drink everytime a player rolls the number 3",
-              style: TextStyle(fontSize: 18),
-            ),
-            Text(
-              "2. The House Elf is the player who has drunk less and bla bla bla...",
-              style: TextStyle(fontSize: 18),
-            ),
-            Text(
-              "3. The King of the castle is the player who has drunk more and...",
-              style: TextStyle(fontSize: 18),
-            ),
-            Text(
-              "4. Everytime someone rolls a total sum of 7, the previous player as to drink and the current player rolls the dice again.",
-              style: TextStyle(fontSize: 18),
-            ),
-            Text(
-              "5. Everytime someone rolls a total sum of 8, everyone has to drink and the current player rolls the dice again ",
-              style: TextStyle(fontSize: 18),
-            ),
-            Text(
-              "6. Everytime someone rolls a total sum of 9, the previous player as to drink and the current player rolls the dice again.",
-              style: TextStyle(fontSize: 18),
-            ),
-            Text(
-              "7. Everytime someone makes people drink three times in a row, a new rule for the game must be set by this player",
-              style: TextStyle(fontSize: 18),
-            ),
-            Text(
-              "8. Everytime that a player rolls double, a duel between two players is set.",
-              style: TextStyle(fontSize: 18),
-            ),
-          ],
+        child: ListView.builder(
+          itemCount: rules.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '\u2022', // Unicode for bullet point
+                    style: TextStyle(fontSize: 18, color: Colors.black),
+                  ),
+                  SizedBox(width: 10), // Spacing between bullet and text
+                  Expanded(
+                    child: Text(
+                      rules[index],
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'SofadiOne',
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
